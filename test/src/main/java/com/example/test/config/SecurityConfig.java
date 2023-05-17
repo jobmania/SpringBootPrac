45569cb65a5dd32bb2986603357b79bea50334a5
Son won cheol <xzxz7003@naver.com>
2023-05-17 16:37:50 +0900

    Spring Security 로그인 (디버깅 필요 )
    - AuthenticationManger 빈등록. 
    - UserDeatilService 구현하여 user에 대해 이름, 패스워드 권한 부여

--- test/src/main/java/com/example/test/config/SecurityConfig.java
+++ test/src/main/java/com/example/test/config/SecurityConfig.java
@@ -1,8 +1,15 @@
 package com.example.test.config;
 
 
+import com.example.test.user.UserRepository;
+import com.example.test.user.UserRole;
+import com.example.test.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,19 +19,26 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 
 @Configuration
 @EnableWebSecurity // @EnableWebSecurity는 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애너테이션
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private UserService userService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.
-            authorizeRequests().antMatchers("/**").permitAll()
+                authorizeRequests().antMatchers("/**").permitAll()
 
                 // csrf에 대해서 /h2-console 허용
                 .and()
-                    .csrf().ignoringAntMatchers("/h2-console/**")
+                .csrf().ignoringAntMatchers("/h2-console/**")
                 .and()
-                    .headers()
-                    .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+                .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and()
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/");
 
         return http.build();
 
@@ -36,5 +50,10 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
 }
--- test/src/main/java/com/example/test/user/UserController.java
+++ test/src/main/java/com/example/test/user/UserController.java
@@ -51,7 +51,7 @@ public class UserController {
         } catch (DataIntegrityViolationException e) {
             e.printStackTrace();
             // BindingResult.reject(오류코드, 오류메시지)는 특정 필드의 오류가 아닌 일반적인 오류를 등록할때 사용한다.
-            result.reject("signupFailed", "이미 등록된 사용자입니다.");
+            result.reject("signupFailed", "이미 등록된 사용자입니다., ID or Email  ");
             return "signup_form";
         } catch (Exception e){
             e.printStackTrace();
@@ -61,5 +61,13 @@ public class UserController {
 
 
         return "redirect:/";
+    }
+
+    /**
+     * 실제 로그인을 진행하는 @PostMapping 방식의 메서드는 스프링 시큐리티가 대신 처리하므로 직접 구현할 필요가 없다.
+     * */
+    @GetMapping("/login")
+    public String login(){
+        return "login_form";
     }
 }
